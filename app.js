a54f35cbf84d179265caa403898e5ed2dfea5d6b
jmoney <jmoney@and-the-bank.local>
2020-04-16 21:42:10 -0600

    this is a bad commit message I did some stuff

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 $(() => {
   console.log("Here be jquerrrrry", $);
 });
+const apiKey = "uhkey";
+const searchURL = "https://www.googleapis.com/youtube/v3/search";
 
-// function watchForm () {
-// $("form").submit((event) => {
-//   event.preventDefault();
-//   const searchTerm = $("#js-search-term").val();
-//   console.log(searchTerm) ;
-//   const maxResults = $("#js-max-results").val();
-//   getYouTubeVideos(searchTerm, maxResults);
-// }:
-// });
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params).map(
+    (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
+}
+
+function displayResults(responseJson) {
+  $("#results-list").empty();
+
+  for (let i = 0; i < responseJson.items.length; i++) {
+    $("#results-list").append(
+      `<li><h3>${responseJson.items[i].snippet.title}</h3>
+        <p>${responseJson.items[i].snippet.description}</p>
+        <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+        </li>`
+    );
+  }
+  $("#results").removeClass("hidden");
+}
+function getYouTubeVideos(query, maxResults = 10) {
+  const params = {
+    key: apiKey,
+    q: query,
+    part: "snippet",
+    maxResults,
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + "?" + queryString;
+
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => displayResults(responseJson))
+    .catch((meow) => {
+      $("#js-error-message").text(`Something went wrong: ${meow.message}`);
+    });
+}
+
+$("form").submit((event) => {
+  event.preventDefault();
+  const searchTerm = $("#js-search-term").val();
+  const maxResults = $("#js-max-results").val();
+  getYouTubeVideos(searchTerm, maxResults);
+});
