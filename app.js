68bc572b1ec41977ec652a09e31de99a6c953053
jmoney <jmoney@and-the-bank.local>
2020-04-16 20:43:00 -0600

    initial commit added html,css,js,jquery, 🥳

---
+++ app.js
@@ -0,0 +1,13 @@
+$(() => {
+  console.log("Here be jquerrrrry", $);
+});
+
+// function watchForm () {
+// $("form").submit((event) => {
+//   event.preventDefault();
+//   const searchTerm = $("#js-search-term").val();
+//   console.log(searchTerm) ;
+//   const maxResults = $("#js-max-results").val();
+//   getYouTubeVideos(searchTerm, maxResults);
+// }:
+// });
